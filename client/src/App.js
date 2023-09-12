b653857c04693d98b96955f4cebab3d185c2b9bb
Pablo Yocupicio <Pabloey@yahoo.com>
2023-09-11 19:14:34 -0700

    Installed axios and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 
 function App () {
+  const testArray = ["item1", "item2", "item3", "item4", "item5"]
   return (
-    <></>
+    <>
+    {testArray.map((e,i) => (
+      <span>{e}</span>
+    ))}
+    </>
   );
 }
 
